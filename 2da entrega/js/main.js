4d128111c22c9a038a267f4345f75da6c180d140
Pablo Gonzalez <gonzalez.e.pablo@gmail.com>
2022-08-23 23:06:52 -0300

    2da Entrega - Simulador interactivo v.2

--- 2da entrega/js/main.js
+++ 2da entrega/js/main.js
@@ -1,16 +1,19 @@
 let sueldoBruto = parseFloat(prompt("Ingresa tu sueldo bruto!"));
+let sueldoNeto = descuentoxLey(sueldoBruto);
 
 function descuentoxLey(sueldoB) {
   if (isNaN(sueldoB)) {
     alert("Tenes que ingresar tu sueldo en numeros!!!");
   } else {
+    let descuentoTotal = 0;
+    let sueldoNeto = 0;
     let input = prompt(
       "Selecciona segun corresponda: \n 1- ¿Pagas aportes sin gremializar? \n 2- ¿Pagas aportes y estas gremializado?"
     );
     switch (input) {
       case "1":
-        let descuentoTotal = (sueldoB * 19) / 100;
-        let sueldoNeto = sueldoB - descuentoTotal;
+        descuentoTotal = (sueldoB * 19) / 100;
+        sueldoNeto = sueldoB - descuentoTotal;
         console.log("Tu sueldo neto es de: $" + sueldoNeto);
         alert(
           "Se te realizo un descuento del 19% y te quedo en mano: $" +
@@ -18,21 +21,33 @@ function descuentoxLey(sueldoB) {
         );
         break;
       case "2":
-        let descuentoTotalgremial = (sueldoB * 19.5) / 100;
-        let sueldoNetogremial = sueldoB - descuentoTotalgremial;
-        console.log("Tu sueldo neto es de: $" + sueldoNetogremial);
+        descuentoTotal = (sueldoB * 19.5) / 100;
+        sueldoNeto = sueldoB - descuentoTotal;
+        console.log("Tu sueldo neto es de: $" + sueldoNeto);
         alert(
           "Se te realizo un descuento del 19.5% y te quedo en mano: $" +
-            sueldoNetogremial
+            sueldoNeto
         );
         break;
       default:
         alert("Fijate donde pones los dedos!!!");
     }
+    return sueldoNeto;
   }
 }
 
-descuentoxLey(sueldoBruto);
+function printSueldo() {
+  if (isNaN(sueldoNeto)) {
+    document.write("Ingresa bien tu sueldo bruto");
+  } else if (sueldoNeto === 0) {
+    document.write("Algo cargaste mal!");
+  } else {
+    document.write(sueldoNeto);
+  }
+}
+
 if (sueldoBruto >= 280000) {
   console.log("Vas a pagar impuesto a la ganancia");
+} else if (sueldoBruto < 280000 && sueldoNeto > 1) {
+  console.log("Safaste de pagar impuesto a la ganancia");
 }
